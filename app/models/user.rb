f7899cce181954b0bfbad9957e11c6b7309aa34e
George <gkatsaros16@gmail.com>
2016-10-12 20:24:49 -0400

    added form for user profile, created routes for edit, update, migration for avatar/bio for users, edit/show view pages, added helper method for image preview/default image.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  
   validates :user_name, presence: true, length: { minimum: 4, maximum: 12 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
