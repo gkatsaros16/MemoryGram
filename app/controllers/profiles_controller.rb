bffff2ed01a2203bf53d0143c21f5d25f03725b4
George <gkatsaros16@gmail.com>
2016-10-13 20:21:41 -0400

    idk what i broke

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,5 +10,21 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    @user = User.find_by(user_name: params[:user_name])
+    # @avatar = User.find_by(user_name: params[:user_name]).avatar
+    if @avatar.update(profile_params)
+      flash[:success] = "Updated successfully!"
+      redirect_to profiles_path(@user.user_name)
+    else
+      @user.errors.full_messages
+      flash[:error] = @user.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:user).permit(:avatar, :bio)
   end
 end
