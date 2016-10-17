eb3518899afc2088472ad3f398339aa5ad7f880a
George <gkatsaros16@gmail.com>
2016-10-16 21:11:55 -0400

    added css, fixed private setuser method, fixed class names on profile helper method

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,20 +1,20 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+  before_action :owned_profile, only: [:edit, :update]
+
 
   def show
-    @user = User.find_by(user_name: params[:user_name])
     @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
   end
 
   def edit
-    @user = User.find_by(user_name: params[:user_name])
   end
 
   def update
-    @user = User.find_by(user_name: params[:user_name])
-    # @avatar = User.find_by(user_name: params[:user_name]).avatar
-    if @avatar.update(profile_params)
+    if @user.update(profile_params)
       flash[:success] = "Updated successfully!"
-      redirect_to profiles_path(@user.user_name)
+      redirect_to profile_path(@user.user_name)
     else
       @user.errors.full_messages
       flash[:error] = @user.errors.full_messages
@@ -26,5 +26,16 @@ class ProfilesController < ApplicationController
 
   def profile_params
     params.require(:user).permit(:avatar, :bio)
+  end
+
+  def set_user
+    @user = User.find_by(user_name: params[:user_name])
+  end
+
+  def owned_profile
+    unless current_user == @user
+      flash[:alert] = "That profile doesn't belong to you!"
+      redirect_to root_path
+    end
   end
 end
