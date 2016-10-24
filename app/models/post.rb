4392cc3209d0b2a4ddc4744b21f35c35d4e8e9f8
George <gkatsaros16@gmail.com>
2016-10-24 12:55:26 -0400

    completed following/followers relations, refactored views into partials, follow view, browse view.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
 
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :notifications, dependent: :destroy 
+  has_many :notifications, dependent: :destroy
 
   validates :user_id, presence: true
   validates :image, presence: true
@@ -13,4 +13,6 @@ class Post < ActiveRecord::Base
 
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  scope :of_followed_users, -> (following_users) { where user_id: following_users }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'browse', to: 'posts#browse', as: :browse_posts  
+
   post ':user_name/follow_user', to: 'relationships#follow_user', as: :follow_user
   post ':user_name/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
 
